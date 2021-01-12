271e8cba1bcc905431f7156fd74cb74e28618b42
Josh Bodah <joshuabodah@gmail.com>
2021-01-12 04:13:09 -0500

    remove unused files

--- Game.gd
+++ Game.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
 func _ready():
-	$BootLoader/TestUI.connect("pressed", get_tree(), "change_scene", ["res://Debug/TestUI.tscn"])
 	$BootLoader/Level1.connect("pressed", get_tree(), "change_scene", ["res://Levels/Level1.tscn"])
