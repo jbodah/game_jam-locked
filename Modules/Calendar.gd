5c9454e194f2158ac57762f8dad3dc64defdd5f8
Josh Bodah <joshuabodah@gmail.com>
2021-01-13 20:27:58 -0500

    add Calendar

---
+++ Modules/Calendar.gd
@@ -0,0 +1,21 @@
+extends "res://Modules/BaseModule.gd"
+
+class_name Calendar
+
+const SPEED = 2
+
+func _initialize(spec):
+	$SimpleMessage.initialize({"messages": spec.messages})
+
+func _ready():
+	$AnimationPlayer.playback_speed = SPEED
+	$AnimationPlayer.play("fly_up_in")
+	$AnimationPlayer.connect("animation_finished", self, "on_animation_finished")
+
+func _process(_delta):
+	if just_clicked() || just_hit_enter():
+		$AnimationPlayer.play("fly_up_out")
+
+func on_animation_finished(name):
+	if name == "fly_up_out":
+		close()
--- Util/SpecToModule.gd
+++ Util/SpecToModule.gd
@@ -3,6 +3,7 @@ const PasswordMessage = preload("res://Modules/Messages/PasswordMessage.tscn")
 const MultiPhaseMessage = preload("res://Modules/Messages/MultiPhaseMessage.tscn")
 const StickyNote = preload("res://Modules/StickyNote.tscn")
 const Google = preload("res://Modules/SearchEngine/Google.tscn")
+const Calendar = preload("res://Modules/Calendar.tscn")
 
 static func spec_to_module(spec):
 	match spec["type"]:
@@ -16,5 +17,7 @@ static func spec_to_module(spec):
 			return StickyNote.instance()
 		"search_engine":
 			return Google.instance()
+		"calendar":
+			return Calendar.instance()
 		_:
 			print("SpecToModule.spec_to_module: unrecognized type ", spec["type"])
