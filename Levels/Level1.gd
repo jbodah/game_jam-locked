357473204790a83bdfe65d6f954d311096735cf0
Josh Bodah <joshuabodah@gmail.com>
2021-01-14 20:47:28 -0500

    remove redundant Level1 script

--- Levels/Level1.gd
+++
@@ -1,9 +0,0 @@
-extends "res://Levels/BaseLevel.gd"
-
-const SpecCompiler = preload("res://SpecCompiler.gd")
-
-func _ready():
-	for spec in specs():
-		if spec.id == "_intro":
-			$Core.play_spec(spec)
-			break
