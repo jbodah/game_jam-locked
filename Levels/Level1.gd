be67ca30771fff021d61638d2a42d6e2258f14ae
Josh Bodah <joshuabodah@gmail.com>
2021-01-16 14:38:01 -0500

    fix path bug in l1

--- Levels/Level1.gd
+++ Levels/Level1.gd
@@ -3,10 +3,10 @@ extends "res://Levels/BaseLevel.gd"
 var angry_range = 5
 
 func _ready():
-	var init_x = $lvl1/Danny.position.x
+	var init_x = $level/Danny.position.x
 	var animation = Animation.new()
 	var track_index = animation.add_track(Animation.TYPE_VALUE)
-	animation.track_set_path(track_index, "lvl1/Danny:position:x")
+	animation.track_set_path(track_index, "level/Danny:position:x")
 	var start = 0.0
 	var step = 0.05
 	animation.track_insert_key(track_index, start, init_x)
