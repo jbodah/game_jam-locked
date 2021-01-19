2204991f43f77b0f10d8910fa418e69d53971786
Josh Bodah <joshuabodah@gmail.com>
2021-01-18 20:29:59 -0500

    edited samples

--- SoundEffect.gd
+++ SoundEffect.gd
@@ -18,30 +18,4 @@ func _ready():
 
 func play(name):
 	print("play sound: %s" % name)
-	if has_method(name):
-		call(name)
-	else:
-		get_node(name).play()
-
-func rummage():
-	play_for($rummage, rand_range(0, 2), 2)
-
-func modem():
-	play_for($modem, 10.1, 2.1)
-
-func cartoon_voice():
-	play_for($cartoon_voice, 0, 0.7)
-	
-func typing():
-	play_for($typing, 0, 1)
-	
-func steve_hey():
-	play_for($steve_hey, 0, 2)
-	
-func coffee():
-	play_for($coffee, 1, 1.5)
-
-func play_for(node, start, duration):
-	node.play(start)
-	yield(get_tree().create_timer(duration), "timeout")
-	node.stop()
+	get_node(name).play()
