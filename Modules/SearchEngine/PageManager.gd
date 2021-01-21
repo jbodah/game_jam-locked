aab62a54fe840ad8eee6ede6a0dd16d43f1358cd
Josh Bodah <joshuabodah@gmail.com>
2021-01-20 21:46:03 -0500

    fixup search engine

--- Modules/SearchEngine/PageManager.gd
+++ Modules/SearchEngine/PageManager.gd
@@ -7,12 +7,9 @@ var search_index = {}
 
 func _init():
 	config = Data.load_config("pages")[1]
-	var sections = config.get_sections()
-	for i in range(sections.size()):
-		var page_id = sections[i]
-		var search_terms = search_terms_for(page_id)
-		for j in range(search_terms.size()):
-			index_search_term(search_terms[j], page_id)
+	for page_id in config.get_sections():
+		for search_term in search_terms_for(page_id):
+			index_search_term(search_term, page_id)
 
 func get_page(page_id):
 	var section_keys = config.get_section_keys(page_id)
@@ -23,26 +20,30 @@ func get_page(page_id):
 	return rv
 
 func index_search_term(search_term, page_id):
+	print("indexing search term; page_id=%s search_term=%s" % [page_id, search_term])
 	var value = search_index.get(search_term, [])
 	value.push_back(page_id)
 	search_index[search_term] = value
 
 func search(text):
-	var results = search_index.get(text, [])
-	var rv = []
-	for i in range(results.size()):
-		rv.push_back(get_page(results[i]))
-	return rv
+	var results = {}
+	for word in sanitize(text).split(" "):
+		for result in search_index.get(word, []):
+			var page = get_page(result)
+			results[page.page_id] = page
+	return results.values()
 
 func search_terms_for(page_id):
 	var set = {}
-	var desc_text = config.get_value(page_id, "desc_text")
-	var keys = sanitize(desc_text).split(" ")
-	for i in range(keys.size()):
-		set[keys[i]] = true
+	var indexed_fields = ["desc_text", "link_text", "header_text"]
+	for field in indexed_fields:
+		var field_text = config.get_value(page_id, field)
+		var keys = sanitize(field_text).split(" ")
+		for key in keys:
+			set[key] = true
 	return set.keys()
 
 func sanitize(string):
 	var regexp = RegEx.new()
-	regexp.compile("[^A-Za-z0-9 ]")
-	return regexp.sub(string, " ", true)
+	regexp.compile("[^a-z0-9 ]")
+	return regexp.sub(string.to_lower(), " ", true)
